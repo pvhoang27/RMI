664ed71f7d6206922129309715dd246cb63a370e
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-11-10 07:56:32 +0700

    first commit

---
+++ src/Byte/rfX6FSCd_B21DCCN393.java
@@ -0,0 +1,22 @@
+package RMI.Byte;
+import java.rmi.*;
+import java.rmi.registry.*;
+import RMI.ByteService;
+
+public class rfX6FSCd_B21DCCN393 {
+    public static void main(String[] args) throws Exception {
+        // a. Nhận dữ liệu
+        ByteService sv = (ByteService) LocateRegistry.getRegistry("203.162.10.109", 1099).lookup("RMIByteService");
+        byte[] a = sv.requestData("B21DCCN393", "rfX6FSCd");
+        for (byte x : a) System.out.print(x + " ");
+        System.out.println();
+
+        // b. Mã hóa Caesar (dịch mỗi byte theo độ dài mảng)
+        for (int i = 0, n = a.length; i < n; i++) a[i] = (byte) (((a[i] & 0xFF) + n) & 0xFF);
+        for (byte x : a) System.out.print(x + " ");
+        System.out.println();
+
+        // c. Gửi dữ liệu mã hóa về server
+        sv.submitData("B21DCCN393", "rfX6FSCd", a);
+    }
+}
