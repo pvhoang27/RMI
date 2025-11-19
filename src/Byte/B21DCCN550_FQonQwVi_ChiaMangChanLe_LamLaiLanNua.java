2f862da961287427d710f615d795e4a1a853f0cc
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-11-19 12:36:57 +0700

    update

---
+++ src/Byte/B21DCCN550_FQonQwVi_ChiaMangChanLe_LamLaiLanNua.java
@@ -0,0 +1,42 @@
+package Byte;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import RMI.ByteService;
+import java.rmi.*;
+import java.rmi.registry.*;
+
+/**
+ *
+ * @author hoang
+ */
+public class B21DCCN550_FQonQwVi_ChiaMangChanLe_LamLaiLanNua {
+    public static void main(String[] args) throws Exception{
+        String studentCode = "B21DCCN550",qCode = "FQonQwVi";
+        Registry rg = LocateRegistry.getRegistry("203.162.10.109");
+        ByteService sv = (ByteService) rg.lookup("RMIByteService");
+        byte[] a = sv.requestData(studentCode, qCode);
+//        System.out.println(a);
+        int index = 0;
+        byte[] kq = new byte[a.length];
+        // dùng thử kiểu này thấy cũng hay hay mà ngắn gọn
+        for(var x : a ){
+            if( x % 2 == 0 ){
+                kq[index] = x;
+                index ++;
+            }
+        }
+        System.out.println(index);
+        for(var x : a ){
+            if( x % 2 != 0 ){
+                kq[index] = x;
+                index ++;
+            }
+        }
+        System.out.println(index);
+        sv.submitData(studentCode, qCode, kq);
+        
+    }
+}
